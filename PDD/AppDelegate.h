a7b3625a828170ff508d4d5096ac6c37ad23cd8f
Sergey Lyubeznov <moltuc@iMac-Sergey.local>
2015-08-27 23:36:33 +0300

    Initial Commit

---
+++ PDD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDD
+//
+//  Created by Sergey Lyubeznov on 27/08/15.
+//  Copyright (c) 2015 Sergey Lyubeznov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
